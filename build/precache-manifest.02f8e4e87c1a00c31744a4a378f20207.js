80bb89b41b1249d3cd7338be4d20e0014ec4caab
Rai Phairow <rai.phairow@gmail.com>
2020-07-01 02:31:31 -0700

    fix tracks

---
+++ build/precache-manifest.02f8e4e87c1a00c31744a4a378f20207.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a964308b02f93758878ce013c81f2f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15482489bb9ba6be9c23",
+    "url": "/static/css/2.b43cacbf.chunk.css"
+  },
+  {
+    "revision": "15482489bb9ba6be9c23",
+    "url": "/static/js/2.85333974.chunk.js"
+  },
+  {
+    "revision": "9f55a1ac102148e41f3a9543b31fc30f",
+    "url": "/static/js/2.85333974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b22f46e632c92fd4e6a3",
+    "url": "/static/js/main.6341dc34.chunk.js"
+  },
+  {
+    "revision": "62be1e37461b4c73af45",
+    "url": "/static/js/runtime-main.ab9c03d4.js"
+  },
+  {
+    "revision": "fcad69eac9110ee7762931fffdc94235",
+    "url": "/static/media/background.fcad69ea.png"
+  },
+  {
+    "revision": "85dc561e40815242b27bfd9f6e68ddbd",
+    "url": "/static/media/logo.85dc561e.svg"
+  },
+  {
+    "revision": "547f1a6631d8364521c0516679cc88a8",
+    "url": "/static/media/screenshot.547f1a66.png"
+  }
+]);
